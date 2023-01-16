18beaf0814d8202f8530ce85aba531ab02d97d04
alorodri <aloro2392@gmail.com>
2023-01-16 17:30:20 +0100

    day5 - b solution

--- AdventOfCode2022/day5/Solver.cs
+++ AdventOfCode2022/day5/Solver.cs
@@ -4,12 +4,11 @@ namespace AdventOfCode2022.day5
 {
     [ProblemDay("day5")]
     [ReturnType(typeof(string), typeof(string))]
-    [TestResult("CMZ")]
+    [TestResult("CMZ", "MCD")]
     internal class Solver : Problem
     {
         public override T Solve<T>(ProblemChoice pc)
         {
-            if (pc == ProblemChoice.B) throw new NotImplementedException();
             List<string> lines = ReadLinesAs<string>();
             List<List<string>> crates = new();
             List<string> orders = new();
@@ -52,10 +51,14 @@ namespace AdventOfCode2022.day5
                 var (from, ptr2) = GetUntilSpaceAs<int>(order, ptr + 4);
                 var (to, _)= GetUntilSpaceAs<int>(order, ptr2 + 2);
 
+                var innerCrate = crates[from - 1];
+                List<string> cratesToMove = innerCrate.TakeLast(quantity).ToList();
+                if (pc == ProblemChoice.B) cratesToMove.Reverse();
+
                 for (int i = 0;  i < quantity; i++)
                 {
-                    var innerCrate = crates[from - 1];
-                    var item = innerCrate[innerCrate.Count - 1];
+                    var item = cratesToMove[cratesToMove.Count - 1];
+                    cratesToMove.RemoveAt(cratesToMove.Count - 1);
                     innerCrate.RemoveAt(innerCrate.Count - 1);
                     crates[to - 1].Add(item);
                 }
